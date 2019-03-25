1ee733ed45aa0da3eba5e8f282ba4ca5ab7c0959
rayman51 <ray.mannionit2@gmail.com>
2019-03-25 19:42:22 +0000

    added destroy script for particles

---
+++ KinectBreaker/Assets/Scripts/TimedDestroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour
+{
+
+    public float destroyTime = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+
+  
+}

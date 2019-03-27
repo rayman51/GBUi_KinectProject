29babe54126a20b091cb00f2de360cb18fab7788
rayman51 <ray.mannionit2@gmail.com>
2019-03-27 16:47:34 +0000

    added KM script to add kinect features

---
+++ KinectBreaker/Assets/Scripts/KM.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Windows.Kinect;
+using UnityEngine;
+
+using UnityEngine.UI;
+using System.Linq;
+
+
+public class KM : MonoBehaviour
+{
+    private KinectSensor sensor;
+    private BodyFrameReader bodyFrameReader;
+    private Body[] bodies = null;
+
+    public GameObject kinectAvailabeText;
+    public Text handXTest;
+
+    public bool IsAvailable;
+    public float PaddlePosition;
+    public bool IsFire;
+
+    public static KM instance = null;
+
+
+    public Body[] GetBobies()
+    {
+        return bodies;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sensor = KinectSensor.GetDefault();
+
+        if (sensor != null)
+        {
+            IsAvailable = sensor.IsAvailable;
+            kinectAvailabeText.SetActive(IsAvailable);
+            bodyFrameReader = sensor.BodyFrameSource.OpenReader();
+
+            if (!sensor.IsOpen)
+            {
+                sensor.Open();
+            }// if
+
+            bodies = new Body[sensor.BodyFrameSource.BodyCount];
+
+        }// if
+    }// start
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

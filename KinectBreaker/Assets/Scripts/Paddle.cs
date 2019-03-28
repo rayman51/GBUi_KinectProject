8f5a3ca993be78e374b3175b7f9b410cbe36c973
rayman51 <ray.mannionit2@gmail.com>
2019-03-28 22:12:28 +0000

    built custom gestures and added gestureDB

--- KinectBreaker/Assets/Scripts/Paddle.cs
+++ KinectBreaker/Assets/Scripts/Paddle.cs
@@ -5,16 +5,40 @@ using UnityEngine;
 public class Paddle : MonoBehaviour {
 
     public float paddleSpeed = 1f;
-
+    public bool turnLeft;
+    public bool turnRight;
     private Vector3 playerPos = new Vector3(0, -9.5f, 0);
 
 
 	// Update is called once per frame
 	void Update () {
 
-        float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
-        playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);// clamps paddle within walls
-        transform.position = playerPos;
+        if (turnLeft)
+        {
+
+            float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+            playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);// clamps paddle within walls
+            transform.position = playerPos;
+            // transform.Rotate(new Vector3(0, -20, 0) * Time.deltaTime);
+            //transform.Rotate(Vector3.right * Time.deltaTime * rotatespeed);
+
+
+        }
+        if (turnRight)
+        {
+            float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+            playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);// clamps paddle within walls
+            transform.position = playerPos;
+            //transform.Rotate(new Vector3(0, 20, 0) * Time.deltaTime);
+        }
+
+
+
+
+
+        //float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+        //playerPos = new Vector3(Mathf.Clamp(xPos, -8f, 8f), -9.5f, 0f);// clamps paddle within walls
+        //transform.position = playerPos;
 
     }// Update
 
